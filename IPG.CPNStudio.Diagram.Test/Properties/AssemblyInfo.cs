e4922438f6c8c301ff4d85d04a901cb0f7e3dd0f
Othman Shuqair <othmanshuqair@hotmail.com>
2019-01-21 13:22:56 +0200

    create basic project skeleton

---
+++ IPG.CPNStudio.Diagram.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IPG.CPNStudio.Diagram.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IPG.CPNStudio.Diagram.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("568140f0-275a-4ae8-8e2b-31f3f9306190")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ IPG.CPNStudio.Diagram.Test/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using IPG.CPNStudio.Diagram;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IPG.CPNStudio.Diagram.Tests
+{
+    [TestClass()]
+    public class UnitTest1
+    {
+       
+    }
+}
+
+namespace IPG.CPNStudio.Diagram.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod()]
+        public void DoSomethingTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
